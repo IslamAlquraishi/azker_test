d29800a3324c923162ebf049a7222ea83bd62b30
IslamAl-Quraishi <eslamalsayed8133@gmail.com>
2023-01-16 02:17:05 +0200

    application azker not finish yet

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ let count = 1;
 let minus_count = 1;
 
 brief_btn.addEventListener("click", () => {
+  get();
   brief_btn.innerHTML = count;
   count++;
 
@@ -17,7 +18,6 @@ brief_btn.addEventListener("click", () => {
   if (brief_count.innerHTML > 0) {
     brief_count.innerHTML -= minus_count;
   } else brief_count.innerHTML = 0;
-  get();
 });
 function get() {
   navigator.vibrate([50, 100]);
@@ -25,7 +25,9 @@ function get() {
 
 refresh.addEventListener("click", () => {
   brief_btn.innerHTML = 0;
+  count = 1;
   brief_count.innerHTML = 3;
+  minus_count = 1;
 });
 
 function get() {
